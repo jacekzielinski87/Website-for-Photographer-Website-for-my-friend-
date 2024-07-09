ee76fa9fb1fa7103d0a7da7ef780c6dca92f1bf0
Jacek Zielinski <jacekzielinski0787@gmail.com>
2024-07-09 11:50:09 +0200

    Gallery section new properties

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,12 +1,16 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react';
 import { slides } from '../data';
+import Lightbox from 'yet-another-react-lightbox';
+import { Captions, Counter } from 'yet-another-react-lightbox/plugins';
+import "yet-another-react-lightbox/plugins/counter.css";
+import "yet-another-react-lightbox/plugins/captions.css";
+import 'yet-another-react-lightbox/styles.css';
 import Photo1 from '../assets/photo2.jpg';
 import Photo2 from '../assets/photo3.jpg';
 import Photo3 from '../assets/Luisa.jpg';
 import Header from './Header';
-import Lightbox from 'yet-another-react-lightbox';
-import 'yet-another-react-lightbox/styles.css';
+
 
 
 const Gallery = () => {
@@ -15,17 +19,32 @@ const Gallery = () => {
 return (
     <>
     <Header/>
-    <h2 className='text-4xl text-white items-center justify-center flex py-8 font-LibreFranklin'>Galeria</h2>
     <section className="flex justify-around m-8">
+      <div>
         <img className='border-4 border-slate-300 rounded-xl opacity-55 transition duration-300 ease-in-out hover:opacity-100' src={Photo1}/>
+        <p className='text-white flex justify-center text-2xl'>Uchwyć wspomnienia</p>
+      </div>
+      <div>
         <img className='border-4 border-slate-300 rounded-xl opacity-55 transition duration-300 ease-in-out hover:opacity-100' src={Photo2}/>
+        <p className='text-white flex justify-center text-2xl'>Uchwyć wspomnienia</p>
+      </div>
+      <div>
         <img className='border-4 border-slate-300 rounded-xl opacity-55 transition duration-300 ease-in-out hover:opacity-100' src={Photo3}/>
+        <p className='text-white flex justify-center text-2xl'>Uchwyć wspomnienia</p>
+      </div>
     </section>
-    <div className=' flex justify-center m-30'>
-    <button onClick={() => setOpen(true)} className=' bg-zinc-800 font-Lexend text-3xl text-white hover hover:bg-zinc-700 border-2 rounded-md p-2'>Sprawdź</button>
-      <Lightbox className='h-2/3'
+    <div className='flex justify-center'>
+    <button onClick={() => setOpen(true)} className=' bg-zinc-800 font-Lexend text-3xl text-white hover hover:bg-zinc-700 border-2 rounded-md p-4 m-24 flex'>Otwórz galerie</button>
+      <Lightbox className='h-screen'
+        plugins={[Captions, Counter]}
+        captions={{
+          showToggle:true,
+          descriptionTextAlign:'center',
+          }}
+        counter={{ container: { style: { top: "unset", bottom: 0 } } }}
         open = {open}
         slides={slides}
+        close={() =>setOpen(false)}
       />
     </div>
     </>
--- src/data.jsx
+++ src/data.jsx
@@ -1,24 +1,31 @@
 export const slides = [
     {
-        src:"https://images.unsplash.com/photo-1718809070481-a16828fbb61d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        src:"https://images.unsplash.com/photo-1718809070481-a16828fbb61d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description:"Train in the city"
     },
     {
-        src:"https://images.unsplash.com/photo-1720170494675-e2dcd6de34a7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        src:"https://images.unsplash.com/photo-1720170494675-e2dcd6de34a7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description:"Stuff at work"
     },
     {
-        src:"https://images.unsplash.com/photo-1720264715773-ec63cbb81e52?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        src:"https://images.unsplash.com/photo-1720264715773-ec63cbb81e52?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description:"Do you wanna ride ?"
     },
     {
-        src:"https://images.unsplash.com/photo-1720264714569-c22a8719abc0?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        src:"https://images.unsplash.com/photo-1720264714569-c22a8719abc0?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description:"Beatiful landscpace"
     },
     {
-        src:"https://images.unsplash.com/photo-1720246153907-565fb9371ff2?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        src:"https://images.unsplash.com/photo-1720246153907-565fb9371ff2?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description:"Chinatown at morning"
     },
     {
-        src:"https://images.unsplash.com/photo-1720264715417-eee343d0d756?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        src:"https://images.unsplash.com/photo-1720264715417-eee343d0d756?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description:"Some rainy day"
     },
     {
-        src:"https://i.postimg.cc/CLJxvkq0/Coffe.jpg"
+        src:"https://i.postimg.cc/CLJxvkq0/Coffe.jpg",
+        description:"Coffe break"
     },
 ]
     
